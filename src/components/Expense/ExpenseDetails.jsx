99db647ab16925705eebb103b22a66137f2e1ee0
kiransalve <salvekiran2011@gmail.com>
2022-09-28 23:15:02 +0530

    Delete button added

--- src/components/Expense/ExpenseDetails.jsx
+++ src/components/Expense/ExpenseDetails.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 const ExpenseDetails = (props) => {
+    let title = props.title
+    const callHandler = () => {
+        title = "Updated"
+        console.log(title)
+    }
+    const deleteHandler = () => {
+        title = "delete button clicked..!!!"
+        console.log(title)
+    }
     return (
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <h2>{props.location}</h2>
             <div className="expense-item__price">$ {props.amount}</div>
+            <button onClick={callHandler}>Change Title</button>
+            <button onClick={deleteHandler}>Delete Title</button>
         </div>
     )
 }

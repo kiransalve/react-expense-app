a1201bd03f24c46fbb9fd74a72f515dc4651b6c8
kiransalve <salvekiran2011@gmail.com>
2022-10-05 17:29:41 +0530

    Event Handlar Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Expenses from "./components/Expense/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   return (
     <div className="App">
+      <NewExpense />
       <Expenses />
     </div>
   );
